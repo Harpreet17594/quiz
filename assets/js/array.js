c6e80ee1eea3b8ca09eecda560376425ea0d9fdd
harpreet kaur <harpreetvirdi17594@gmail.com>
2023-07-29 18:25:38 +0100

    Updated array.js added 4 functions

--- assets/js/array.js
+++ assets/js/array.js
@@ -1,29 +1,22 @@
-// import { setTime,sendMessage } from './js/time.js';
-
-// import { setTime } from "./timer.js";
 var heading = document.getElementById("heading");
 var option1 = document.getElementById("option1");
 var option2 = document.getElementById("option2");
 var option3 = document.getElementById("option3");
 var option4 = document.getElementById("option4");
+
 var show = document.getElementById("show");
-var start_quiz = document.getElementById("start_quiz");
+// var start_quiz = document.getElementById("start_quiz");
 var endScreen = document.getElementById("end-screen");
 var start = document.getElementById("start");
 var finalScore = document.getElementById("final-score");
 
 var counter = 0;
-var answer = "";
-var change = "";
+var answer;
+var change;
 var count_score = 0;
 
 var timeEl = document.querySelector(".time");
 var secondsLeft = 60;
-
-start.addEventListener("click", (event) => {
-  getData(counter);
-  setTime();
-});
 
 heading.style.visibility = "hidden";
 option1.style.visibility = "hidden";
@@ -32,7 +25,7 @@ option3.style.visibility = "hidden";
 option4.style.visibility = "hidden";
 show.style.visibility = "hidden";
 endScreen.style.visibility = "hidden";
-start_quiz.style.visibility = "hidden";
+// start_quiz.style.visibility = "hidden";
 
 var myQuestions = [
   {
@@ -41,9 +34,9 @@ var myQuestions = [
       a: "Hypertext Markup Language",
       b: "Hypertext Markup Loop",
       c: "Hypertext Language",
-      d: "Hypertext Language",
+      d: "Language",
     },
-    correctAnswer: "c",
+    correctAnswer: "a",
   },
   {
     question: "What is the full form of CSS? ",
@@ -58,142 +51,188 @@ var myQuestions = [
   {
     question: "What is the full form of JAVASCRIPT? ",
     answers: {
-      a: "Cascading style page",
-      b: "Cascading style sheet",
-      c: "Cascading sheet",
-      d: "Hypertext Language",
-    },
-    correctAnswer: "a",
-  },
-  {
-    question: "What is the full form of REACT? ",
-    answers: {
-      a: "Cascading style page",
-      b: "Cascading style sheet",
-      c: "Cascading sheet",
-      d: "Hypertext Language",
+      a: "java",
+      b: "script",
+      c: "java script",
+      d: "js",
     },
     correctAnswer: "c",
   },
   {
-    question: "What is the full form of GIT? ",
+    question: "What is not programming language? ",
     answers: {
-      a: "Cascading style page",
-      b: "Cascading style sheet",
-      c: "Cascading sheet",
-      d: "Hypertext Language",
+      a: "C",
+      b: "C++",
+      c: "firefox",
+      d: "css",
+    },
+    correctAnswer: "c",
+  },
+  {
+    question: "What is GIT? ",
+    answers: {
+      a: "version control system",
+      b: "virus control system",
+      c: "control system",
+      d: "software",
     },
     correctAnswer: "a",
   },
 ];
 
-// start.addEventListener("click",getData(counter));
-
 function getData(counter) {
-  console.log(counter);
   heading.style.visibility = "visible";
   option1.style.visibility = "visible";
   option2.style.visibility = "visible";
   option3.style.visibility = "visible";
   option4.style.visibility = "visible";
   show.style.visibility = "visible";
-  start_quiz.style.visibility = "hidden";
+  //   start_quiz.style.visibility = "hidden";
   start.style.visibility = "hidden";
 
-  if (counter == myQuestions.length) {
-    //it means array is empty now
-    heading.style.visibility = "hidden";
-    option1.style.visibility = "hidden";
-    option2.style.visibility = "hidden";
-    option3.style.visibility = "hidden";
-    option4.style.visibility = "hidden";
-    show.style.visibility = "hidden";
-    start_quiz.style.visibility = "hidden";
-    start.style.visibility = "hidden";
-    endScreen.style.visibility = "visible";
-
-    console.log("count score : " + count_score);
-    //set final score
-    finalScore.textContent = count_score;
-  }
+  answer = myQuestions[counter].correctAnswer;
+  //   show.textContent = "";
 
   heading.textContent = myQuestions[counter].question;
   option1.textContent = myQuestions[counter].answers.a;
-  option1.addEventListener("click", function () {
-    change = "a";
-    a();
-  });
+  option1.addEventListener("click", option1Click);
   option2.textContent = myQuestions[counter].answers.b;
-  option2.addEventListener("click", function () {
-    // event.preventDefault();
-    change = "b";
-    a();
-  });
+  option2.addEventListener("click", option2Click);
   option3.textContent = myQuestions[counter].answers.c;
-  option3.addEventListener("click", function () {
-    change = "c";
-    a();
-  });
+  option3.addEventListener("click", option3Click);
   option4.textContent = myQuestions[counter].answers.d;
-  option4.addEventListener("click", function () {
-    change = "d";
-    a();
-  });
-  answer = myQuestions[counter].correctAnswer;
+  option4.addEventListener("click", option4Click);
+  return;
 }
 
-//-------------
-// start_quiz.addEventListener('style', 'visibility:hidden');
-//--------------
-function a() {
-  if (answer == change) {
-    console.log("data=  " + answer);
-    if (counter < myQuestions.length) {
-      counter = counter + 1;
+function option1Click() {
+  change = "a";
+  if (counter < myQuestions.length) {
+    if (change == answer) {
+      counter += 1;
+      count_score += 1;
+      getData(counter);
     } else {
-      heading.style.display = "none";
-      option1.style.display = "none";
-      option2.style.display = "none";
-      option3.style.display = "none";
-      option4.style.display = "none";
-      show.style.display = "none";
-      start_quiz.style.display = "none";
-      start.style.display = "none";
-      endScreen.style.visibility = "visible";
-
-      console.log("count score : " + count_score);
-      //set final score
-      finalScore.textContent = count_score;
-
-      return;
+      counter += 1;
+      getData(counter);
     }
-    show.textContent = answer + " is the correct answer";
-    count_score = count_score + 1;
-    getData(counter);
+  } else {
+    heading.style.display = "none";
+    option1.style.display = "none";
+    option2.style.display = "none";
+    option3.style.display = "none";
+    option4.style.display = "none";
+    show.style.display = "none";
+    start.style.display = "none";
+    endScreen.style.visibility = "visible";
+    //set final score
+    finalScore.textContent = count_score;
+    return;
   }
-
-  // else if(answer == change)
-  // {
-  // 	console.log("data=  "+answer);
-  // 	counter=counter+1;
-  // 	show.textContent = answer+" is the correct answer";
-  // 	getData(counter);
-
-  // } else if(answer == change)
-  // {
-  // 	console.log("data=  "+answer);
-  // 	counter=counter+1;
-  // 	show.textContent = answer+" is the correct answer";
-  // 	getData(counter);
-
-  // }else if(answer == change)
-  // {
-  // 	console.log("data=  "+answer);
-  // 	counter=counter+1;
-  // 	show.textContent = answer+" is the correct answer";
-  // 	getData(counter);
-  // }
 }
+function option2Click() {
+  change = "b";
+  if (counter < myQuestions.length) {
+    if (change == answer) {
+      counter += 1;
+      count_score += 1;
+      getData(counter);
+    } else {
+      counter += 1;
+      getData(counter);
+    }
+  } else {
+    heading.style.display = "none";
+    option1.style.display = "none";
+    option2.style.display = "none";
+    option3.style.display = "none";
+    option4.style.display = "none";
+    show.style.display = "none";
+    start.style.display = "none";
+    endScreen.style.visibility = "visible";
+    //set final score
+    finalScore.textContent = count_score;
+    return;
+  }
+}
+
+function option3Click() {
+  change = "c";
+  if (counter < myQuestions.length) {
+    if (change == answer) {
+      counter += 1;
+      count_score += 1;
+      getData(counter);
+    } else {
+      counter += 1;
+      getData(counter);
+    }
+  } else {
+    heading.style.display = "none";
+    option1.style.display = "none";
+    option2.style.display = "none";
+    option3.style.display = "none";
+    option4.style.display = "none";
+    show.style.display = "none";
+    start.style.display = "none";
+    endScreen.style.visibility = "visible";
+    //set final score
+    finalScore.textContent = count_score;
+    return;
+  }
+}
+
+function option4Click() {
+  change = "d";
+  if (counter < myQuestions.length) {
+    if (change == answer) {
+      counter += 1;
+      count_score += 1;
+      getData(counter);
+    } else {
+      counter += 1;
+      getData(counter);
+    }
+  } else {
+    heading.style.display = "none";
+    option1.style.display = "none";
+    option2.style.display = "none";
+    option3.style.display = "none";
+    option4.style.display = "none";
+    show.style.display = "none";
+    start.style.display = "none";
+    endScreen.style.visibility = "visible";
+    //set final score
+    finalScore.textContent = count_score;
+    return;
+  }
+}
+
+// function setCount() {
+//   if (counter < myQuestions.length) {
+//     console.log("type of change :" + typeof change);
+//     console.log("type of answer :" + typeof answer);
+//     console.log("counter :" + counter);
+//     console.log("count score :" + count_score);
+
+//     counter = counter + 1;
+//     show.textContent = answer + " is the correct answer";
+
+//     getData(counter);
+//   } else {
+//     heading.style.display = "none";
+//     option1.style.display = "none";
+//     option2.style.display = "none";
+//     option3.style.display = "none";
+//     option4.style.display = "none";
+//     show.style.display = "none";
+//     start.style.display = "none";
+//     endScreen.style.visibility = "visible";
+//     //set final score
+//     finalScore.textContent = count_score;
+//     return;
+//   }
+// }
 
 //timer function
 function setTime() {
@@ -215,3 +254,8 @@ function setTime() {
 function sendMessage() {
   timeEl.textContent = " ";
 }
+
+start.addEventListener("click", (event) => {
+  getData(counter);
+  setTime();
+});
